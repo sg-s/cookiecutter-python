b9d3ffb0bc35f3a84d29bee31495e6b71f9d70d8
Srinivas Gorur-Shandilya <code@srinivas.gs>
2022-12-12 15:27:00 -0500

    ready to test

--- {{cookiecutter.project_name}}/assets/make_conda_env.sh
+++ {{cookiecutter.project_name}}/assets/make_conda_env.sh
@@ -8,13 +8,16 @@ fi
 
 # makes a new conda environment 
 
-# if no 2nd argument is provided, default to 3.6
+# figure out which version of python we should use
 PYVERSION=$(cat setup.py | grep "python_requires"  | tr -dc '0-9.'
 )
 
 echo "Will make environment using this python version:"
 echo $PYVERSION
 
+
+# we will name the conda env after the name of the repo
+# let's sanitize the name a little...
 CONDA_ENV_NAME=$(cat setup.py | grep "name=" | cut -d "=" -f 2)
 CONDA_ENV_NAME=${CONDA_ENV_NAME//'"'/}
 CONDA_ENV_NAME=${CONDA_ENV_NAME//','/}
@@ -23,25 +26,25 @@ CONDA_ENV_NAME=${CONDA_ENV_NAME//'-'/_}
 echo "Will make environment named:"
 echo $CONDA_ENV_NAME
 
-# # conda, in its infinite wisdom, has decided that it won't
-# # work when called from shell scripts, see:
-# # https://github.com/conda/conda/issues/7980
-# #
-# # this inscrutable workaround works, but I have no idea why
-# # 
-# eval "$(conda shell.bash hook)"
+# conda, in its infinite wisdom, has decided that it won't
+# work when called from shell scripts, see:
+# https://github.com/conda/conda/issues/7980
+#
+# this inscrutable workaround works, but I have no idea why
+# 
+eval "$(conda shell.bash hook)"
 
-# # deactivate whatever we're in now
-# conda deactivate 
+# deactivate whatever we're in now
+conda deactivate 
 
-# # first, make a new env without user intervention 
-# yes | conda create -n $1 python=$PYVERSION pip wheel  
-
-
-# # switch to that
-# conda activate $1 
+# first, make a new env without user intervention 
+yes | conda create -n $CONDA_ENV_NAME python=$PYVERSION pip wheel  
 
 
-# # allow this environment to show up in jupyter lab
-# pip install ipykernel
-# python -m ipykernel install --user --name $1
+# switch to that
+conda activate $CONDA_ENV_NAME
+
+
+# allow this environment to show up in jupyter lab
+pip install ipykernel
+python -m ipykernel install --user --name $CONDA_ENV_NAME
